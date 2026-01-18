d03073675ad4eead15af435f6b8879edfdec1705
Trisyefo <syefcodewithlinux@gmail.com>
2026-01-18 20:38:43 +0700

    code

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char input;
+
+char *logo =
+"                                 :.                    \n"
+"                                                       \n"
+"                                                       \n"
+"                       -**:  .+###-                    \n"
+"                      .= :*. -+  -%.                   \n"
+"                       +=-++++*--+=                    \n"
+"                       -==++**++===                    \n"
+"                       .-=====-====  --                \n"
+"                       =**=--=**#%%%                   \n"
+"                      =%%##*##%%%%%%#.                 \n"
+"                     #%%%%%%%%%%%%%%%+                 \n"
+"                    -%%%%%%%%%%%######-                \n"
+"                   :%%%%%%%%%%%%%%%%%%#%   .           \n"
+"                . =%%%%%%%%%%%%%%%%%%%%%#..            \n"
+"                 :%%%%%%%%%%%%%%%%%%%%%%%=   .         \n"
+"                 #%%%%%%%%%%%%%%%%%%%%%%%+   .         \n"
+"              ...%%%%%%%%%%%%%%%%%%%%%%%%+  .          \n"
+"            .--.-%%%%%%%%%%%%%%%%%%%%%%%%=    .-.      \n"
+"          -==+++- :#%%%%%%%%%%%%%%%%%%*=+-      .==    \n"
+"     -=====++++++=.  -#%%%%%%%%%%%%%%#*=+=:  .:=++=    \n"
+"     ==+++++++++++=:   :%%%%%%%%%%%%%#+=++====+++++==+ \n"
+"     -==++++++++++++=  =%%%%%%%%%%%%#..=+++++++++++++==\n"
+"     -=++++++++++++++==%%%%%%%%%%#-   :=+++++++++++==--\n"
+"     -====+++++++++++=-:  .:::.       :=++++++==--:::  \n"
+"      --------========-:              :--===--:::      \n"
+"               :::::::..              ....::..         \n";
+
+int main(){
+	system("clear");
+	printf("\033[32m%s\n\n\033[0m",logo);
+	printf("\033[32mWelcome to Ubuntu setup please select the option below:\n\n\033[0m");
+printf("\033[34m0. Complete Installation\n(Also Includes Git and 4 Programming Language and Vim code editor)\033[0m\n\n1. Update and Upgrade\n2. Update\n3. Upgrade\n\n");
+printf(": ");
+printf("\033[3 q");
+scanf("%c", &input);
+if(input == '0'){
+system("apt -y update && apt -y upgrade && apt -y install git && apt install -y python-is-python3 && apt install -y gcc && apt install -y vim && apt install -y g++ && apt install -y nodejs");
+}
+else if(input ==  '1'){
+system("apt -y update && apt -y upgrade");
+}
+else if(input == '2') {
+system("apt -y update");
+}
+else if(input == '3') {
+system("apt -y upgrade");                                             }
+return 0;
+}
